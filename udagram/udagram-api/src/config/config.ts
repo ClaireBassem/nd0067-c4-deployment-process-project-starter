eff6175d7f268e0f5dfa675c2fbc3539f7a4ecf8
claire-bassem <claire.bassem@thirdwayv.com>
2024-12-24 17:54:41 +0200

    updates in project after successful deployment on aws

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -6,16 +6,33 @@ dotenv.config();
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
+// export const config = {
+//   username: `${process.env.POSTGRES_USERNAME}`,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+//   host: process.env.POSTGRES_HOST,
+//   aws_region: process.env.AWS_REGION,
+//   aws_profile: process.env.AWS_PROFILE,
+//   aws_media_bucket: process.env.AWS_BUCKET,
+//   url: process.env.URL,
+//   jwt: {
+//     secret: process.env.JWT_SECRET,
+//   },
+// };
+
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
-  url: process.env.URL,
+  username: "postgres",
+  password: "postgres1234*",
+  database: "postgres",
+  host: "postgres.cbrtlgy5plc1.us-east-2.rds.amazonaws.com",
+  aws_region: "",
+  AWS_DEFAULT_REGION:"",
+  aws_profile: "",
+  aws_media_bucket: "",
+  url: "http://localhost:8080",
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: "process.env.JWT_SECRET",
   },
+  accessKeyId:"",
+  secretAacessKey:""
 };
